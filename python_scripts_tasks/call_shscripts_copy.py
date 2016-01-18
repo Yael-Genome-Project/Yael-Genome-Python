74921099831bcac1f0b14eaa90dc60240b95ae6a
Connie <herculepoirot24@gmail.com>
2016-01-17 22:54:28 -0500

    py_script_cp_and_run
    The script could copy sh scripts from another folder to another
    location and then run the scripts.

---
+++ python_scripts_tasks/call_shscripts_copy.py
@@ -0,0 +1,19 @@
+import subprocess, sys, shutil
+
+x = sys.argv[1]
+y = "./" + x
+
+src = "/groups/immdiv-bioinfo/evergrande/copy_work/yael/FC_01744/Project_yael/test_runscripts/" + x
+dst = "/groups/immdiv-bioinfo/evergrande/copy_work/yael/FC_01744/Project_yael/mouse_S9-S10/"
+
+shutil.copy(src, dst)
+
+subprocess.call([y])
+
+
+# subprocess.call(["sh","/groups/immdiv-bioinfo/evergrande/copy_work/yael/FC_01744/Project_yael/mouse_S9-S10", y])
+# /groups/immdiv-bioinfo/evergrande/copy_work/yael/FC_01744/Project_yael/mouse_S9-S10: is a directory
+
+#subprocess.call(['./runScript_mouse.sh']) worked!
+
+print ("works!")
